de8acf2e038c3149e179846564be5d217db94dfa
wintvelt <wintvelt@me.com>
2017-02-14 19:34:46 +0100

    remove clutter

--- src/App.elm
+++ src/App.elm
@@ -10,6 +10,7 @@ import Route as Route exposing (Route(..), Page(..))
 import Task
 import Process
 
+
 type alias Model =
     { movies : Dict Int String
     , currentPage : Page
@@ -17,39 +18,41 @@ type alias Model =
     , serverRequest : Maybe Int
     }
 
-type alias Movie = { title : String, year : Int }
 
-mockServerMovies = 
-    Dict.fromList 
-    [ (0, { title = "Abduction", year = 2011})
-    , (1, { title = "Blues Brothers", year = 1980})
-    , (2, { title = "Cat People", year = 1982 })
-    , (4, { title = "Eagle Eye", year = 2008 })
-    ]
+type alias Movie =
+    { title : String, year : Int }
+
+
+mockServerMovies =
+    Dict.fromList
+        [ ( 0, { title = "Abduction", year = 2011 } )
+        , ( 1, { title = "Blues Brothers", year = 1980 } )
+        , ( 2, { title = "Cat People", year = 1982 } )
+        , ( 4, { title = "Eagle Eye", year = 2008 } )
+        ]
 
 
 init : Location -> ( Model, Cmd Msg )
 init location =
-    { movies = 
-        Dict.fromList 
-            [ (0, "Abduction")
-            , (1, "Blues Brothers")
-            , (2, "Cat People")
-            , (3, "Da Vinci Code")
-            , (4, "Eagle Eye")
+    { movies =
+        Dict.fromList
+            [ ( 0, "Abduction" )
+            , ( 1, "Blues Brothers" )
+            , ( 2, "Cat People" )
+            , ( 3, "Da Vinci Code" )
+            , ( 4, "Eagle Eye" )
             ]
     , currentPage = HomePage
     , message = ""
     , serverRequest = Nothing
     }
-    |> urlUpdate location
+        |> urlUpdate location
 
 
 type Msg
     = UrlChanged Location
     | LoadError String
     | GotMovieDetail Movie
-    | TryUrl String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -59,34 +62,31 @@ update msg model =
             urlUpdate newLocation model
 
         LoadError message ->
-            (   { model 
+            ( { model
                 | serverRequest = Nothing
-                , message = message 
-                }
+                , message = message
+              }
             , Cmd.none
             )
 
         GotMovieDetail movie ->
             case model.serverRequest of
                 Just id ->
-                    (   { model 
+                    ( { model
                         | currentPage = MovieDetailPage id movie
                         , serverRequest = Nothing
-                        , message = "" 
-                        }
-                    ,   Route.newUrl <| MovieDetailPage id movie
+                        , message = ""
+                      }
+                    , Route.newUrl <| MovieDetailPage id movie
                     )
 
                 Nothing ->
-                    (   { model 
-                        | message = "got strange stuff" 
+                    ( { model
+                        | message = "got strange stuff"
                         , serverRequest = Nothing
-                        }
-                    ,   Cmd.none
+                      }
+                    , Cmd.none
                     )
-
-        TryUrl hashLess ->
-            model ! [ Navigation.newUrl hashLess ]
 
 
 urlUpdate : Location -> Model -> ( Model, Cmd Msg )
@@ -94,33 +94,32 @@ urlUpdate newLocation model =
     case Route.parse newLocation of
         Nothing ->
             ( { model | message = "invalid URL: " ++ newLocation.hash }
-            , Route.modifyUrl model.currentPage 
+            , Route.modifyUrl model.currentPage
             )
 
         Just validRoute ->
             if Route.isEqual validRoute model.currentPage then
-                ( model , Cmd.none )
-
+                ( model, Cmd.none )
             else
                 case validRoute of
                     Home ->
-                        ( { model | currentPage = HomePage}
+                        ( { model | currentPage = HomePage }
                         , Cmd.none
                         )
 
-
                     Movies ->
-                        ( { model | currentPage = MoviesPage (Dict.toList model.movies)}
+                        ( { model | currentPage = MoviesPage (Dict.toList model.movies) }
                         , Cmd.none
                         )
 
                     MovieDetail id ->
-                        (   { model 
-                            | serverRequest = Just id 
+                        ( { model
+                            | serverRequest = Just id
                             , message = "Loading data for movie : " ++ toString id
-                            } 
+                          }
                         , Cmd.batch [ fetchMovieDetail id, Route.modifyUrl model.currentPage ]
                         )
+
 
 fetchMovieDetail : Int -> Cmd Msg
 fetchMovieDetail id =
@@ -132,12 +131,13 @@ fetchMovieDetail id =
 
                 Just movie ->
                     Task.succeed movie
+
         delayedFetchTask =
             Process.sleep 1000
-            |> Task.andThen (\_ -> fetchTask)
+                |> Task.andThen (\_ -> fetchTask)
     in
-      Task.attempt processFetch delayedFetchTask
-        
+        Task.attempt processFetch delayedFetchTask
+
 
 processFetch : Result String Movie -> Msg
 processFetch result =
@@ -148,92 +148,93 @@ processFetch result =
         Ok movie ->
             GotMovieDetail movie
 
+
 view : Model -> Html Msg
 view model =
-  case model.currentPage of
-    HomePage ->
-        homeView model
+    case model.currentPage of
+        HomePage ->
+            homeView model
 
-    MoviesPage movies ->
-        moviesView model movies
-  
-    MovieDetailPage movieId movie ->
-        moviesDetailView model movieId movie
+        MoviesPage movies ->
+            moviesView model movies
+
+        MovieDetailPage movieId movie ->
+            moviesDetailView model movieId movie
+
 
 homeView : Model -> Html Msg
 homeView model =
-  div [ class "mdl-layout mdl-layout--fixed-header" ]
-    [ div 
-        [ class "mdl-layout__header mdl-layout__header-row" ]
-        [ span [ class "mdl-layout__title" ] [ text "Homepage" ] 
+    div [ class "mdl-layout mdl-layout--fixed-header" ]
+        [ div
+            [ class "mdl-layout__header mdl-layout__header-row" ]
+            [ span [ class "mdl-layout__title" ] [ text "Homepage" ]
+            ]
+        , p [ class "message" ] [ text model.message ]
+        , a
+            [ href <| Route.toUrl Movies
+            , class "mdl-button"
+            ]
+            [ text "Show movielist"
+            , i [ class "material-icons" ] [ text "chevron_right" ]
+            ]
         ]
-    , p [ class "message" ] [ text model.message ]
-    , a 
-        [ href <| Route.toUrl Movies 
-        , class "mdl-button"
-        ] 
-        [ text "Show movielist"
-        , i [ class "material-icons" ] [ text "chevron_right" ]
-        ]
-    , button [ onClick <| TryUrl "/users" ] [ text "Try hashLess" ]
-    , a [ href "/users" ] [ text "Try hashLess link" ]
-    , div [ attribute "onclick" "history.pushState({},'check','/users');"] [ text "try hashless hack"]
-    , a [ href "/users", onClickPrevent <| TryUrl "/users/whatever" ] [ text "Try hashLess link with different msg" ]
-    ]
+
 
 onClickPrevent : msg -> Attribute msg
 onClickPrevent msg =
-    onWithOptions "click" { preventDefault = True, stopPropagation = False} (succeed msg)
+    onWithOptions "click" { preventDefault = True, stopPropagation = False } (succeed msg)
 
 
 moviesView model movies =
-  div [ class "mdl-layout mdl-layout--fixed-header" ]
-    [ div [ class "mdl-layout__header mdl-layout__header-row" ]
-      [ a 
-        [ href <| Route.toUrl Home 
-        , class "mdl-navigation__link"
-        ] 
-        [ i [ class "material-icons" ] [ text "chevron_left" ]
-        , text "HomePage" 
+    div [ class "mdl-layout mdl-layout--fixed-header" ]
+        [ div [ class "mdl-layout__header mdl-layout__header-row" ]
+            [ a
+                [ href <| Route.toUrl Home
+                , class "mdl-navigation__link"
+                ]
+                [ i [ class "material-icons" ] [ text "chevron_left" ]
+                , text "HomePage"
+                ]
+            , span [ class "mdl-layout__title" ] [ text "Movie List" ]
+            ]
+        , p [ class "message" ] [ text model.message ]
+        , ul [ class "mdl-list" ] <|
+            List.map viewMovie movies
         ]
-      , span [ class "mdl-layout__title" ] [ text "Movie List" ]
-      ]
-    , p [ class "message" ] [ text model.message ]
-    , ul [ class "mdl-list" ]
-      <| List.map viewMovie movies
-    ]
 
-viewMovie (id, movie) =
-  li [ class "mdl-list__item" ]  
-    [ span [ class "mdl-list__item-primary-content" ] [ text movie ]
-    , a 
-        [ href <| Route.toUrl <| MovieDetail id 
-        , class "mdl-list__item-secondary-action mdl-button mdl-button--accent"
+
+viewMovie ( id, movie ) =
+    li [ class "mdl-list__item" ]
+        [ span [ class "mdl-list__item-primary-content" ] [ text movie ]
+        , a
+            [ href <| Route.toUrl <| MovieDetail id
+            , class "mdl-list__item-secondary-action mdl-button mdl-button--accent"
+            ]
+            [ text "details"
+            , i [ class "material-icons" ] [ text "chevron_right" ]
+            ]
         ]
-        [ text "details" 
-        , i [ class "material-icons"] [ text "chevron_right" ]
-        ]
-    ]
+
 
 moviesDetailView : Model -> Int -> Movie -> Html msg
 moviesDetailView model movieId movie =
-  div [ class "mdl-layout mdl-layout--fixed-header" ]
-    [ div [ class "mdl-layout__header mdl-layout__header-row" ]
-      [ a 
-        [ href <| Route.toUrl Movies
-        , class "mdl-navigation__link"
+    div [ class "mdl-layout mdl-layout--fixed-header" ]
+        [ div [ class "mdl-layout__header mdl-layout__header-row" ]
+            [ a
+                [ href <| Route.toUrl Movies
+                , class "mdl-navigation__link"
+                ]
+                [ i [ class "material-icons" ] [ text "chevron_left" ]
+                , text "Movies"
+                ]
+            , span [ class "mdl-layout__title" ] [ text "Movie details" ]
+            ]
+        , p [ class "message" ] [ text model.message ]
+        , div [ class "movie-details" ]
+            [ h3 [] [ text movie.title ]
+            , h4 [] [ text <| "year : " ++ toString movie.year ]
+            ]
         ]
-        [ i [ class "material-icons" ] [ text "chevron_left" ]
-        , text "Movies" 
-        ]
-      , span [ class "mdl-layout__title" ] [ text "Movie details" ]
-      ]
-    , p [ class "message" ] [ text model.message ] 
-    , div [ class "movie-details" ]
-      [ h3 [] [ text movie.title ]
-      , h4 [] [ text <| "year : " ++ toString movie.year ]
-      ]
-    ]
 
 
 subscriptions : Model -> Sub Msg
